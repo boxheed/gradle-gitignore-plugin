0d537c9aeaf429d5cf516daaafb0bed668851c35
Andrew Dunn <adunn@mango-solutions.com>
2017-08-23 12:16:17 +0100

    Updated to get rid of deprecation warnings due to the << operator

--- buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/gitignore/GitignorePluginExtension.groovy
+++ buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/gitignore/GitignorePluginExtension.groovy
@@ -2,9 +2,9 @@ package com.fizzpod.gradle.plugins.gitignore;
 
 public class GitignorePluginExtension {
 	List<String> ignores = new ArrayList<String>();
-	
+
 	def ignore(String ignore) {
-		ignores << ignore;
+		ignores.add(ignore);
 		return this;
 	}
 }
